f6e499a7d1fcd2b00f0ce824c1fb0b26cb3a6417
yangfriendship <yangfriendship@naver.com>
2022-04-26 22:23:24 +0900

    JpaBatchItemWriter

--- src/main/java/me/youzheng/springbatch/db/entity/Customer2.java
+++ src/main/java/me/youzheng/springbatch/db/entity/Customer2.java
@@ -5,7 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,23 +17,12 @@ import lombok.Setter;
 @NoArgsConstructor
 @Builder
 @Entity
-public class Customer {
+public class Customer2 {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "firstName")
     private String firstName;
     private String lastName;
     private String birthdate;
 
-    @Override
-    public String toString() {
-        return "Customer{" +
-            "id=" + id +
-            ", firstName='" + firstName + '\'' +
-            ", lastName='" + lastName + '\'' +
-            ", birthdate='" + birthdate + '\'' +
-            '}';
-    }
 }
